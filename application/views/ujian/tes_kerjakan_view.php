189ffa897dcc1a49a1c1a2f0003f9d613a5890a7
Tintapen <okiper12@gmail.com>
2021-02-26 12:59:27 +0700

    remove variable inside in setInterval x

--- application/views/ujian/tes_kerjakan_view.php
+++ application/views/ujian/tes_kerjakan_view.php
@@ -357,11 +357,6 @@
 
             var distance = waktuSelesai - now;
 
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
             if (distance < 0) {
                 clearInterval(x)
                 window.location = '<?php echo site_url('tes_dashboard') ?>';
