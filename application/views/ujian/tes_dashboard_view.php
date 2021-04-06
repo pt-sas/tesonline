a2f97e73da1a2dfb4a5d6d842628bce75efff29b
Tintapen <okiper12@gmail.com>
2021-04-06 11:10:58 +0700

    Add function realoadTable and Button Refresh

--- application/views/ujian/tes_dashboard_view.php
+++ application/views/ujian/tes_dashboard_view.php
@@ -1,25 +1,33 @@
 <div class="container">
-	<!-- Content Header (Page header) -->
+    <!-- Content Header (Page header) -->
     <section class="content-header">
-    	<h1>
-    		Selamat Datang,  <?php if(!empty($nama)){ echo $nama; } if(!empty($group)){ echo ' ('.$group. ')';} ?>
+        <h1>
+            Selamat Datang, <?php if (!empty($nama)) {
+                                echo $nama;
+                            }
+                            if (!empty($group)) {
+                                echo ' (' . $group . ')';
+                            } ?>
             <small>Rekrutmen Online PT. Sahabat Abadi Sejahtera</small>
         </h1>
         <ol class="breadcrumb">
-        	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
             <li class="active">dashboard</li>
         </ol>
-	</section>
+    </section>
 
-	<!-- Main content -->
+    <!-- Main content -->
     <section class="content">
         <div class="callout callout-info">
             <h4>Informasi</h4>
             <p>Silahkan pilih Tes yang diikuti dari daftar tes yang tersedia dibawah ini. Apabila tes tidak muncul, silahkan menghubungi Team HRD kami.</p>
         </div>
-        <div class="box box-primary box-solid">
+        <div class="box box-primary">
             <div class="box-header with-border">
                 <h3 class="box-title">Daftar Tes</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-primary btn-sm" onclick="reloadTable()"><i class="fa fa-refresh"> Refresh</i></button>
+                </div>
             </div><!-- /.box-header -->
             <div class="box-body">
                 <table id="table-tes" class="table table-bordered table-hover">
@@ -43,30 +51,60 @@
                             <td> </td>
                         </tr>
                     </tbody>
-                </table>   
+                </table>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
     </section><!-- /.content -->
 </div><!-- /.container -->
 
 <script type="text/javascript">
-    $(function () {
+    $(function() {
         $('#table-tes').DataTable({
-                  "paging": true,
-                  "iDisplayLength":10,
-                  "bProcessing": false,
-                  "bServerSide": true, 
-                  "searching": false,
-                  "aoColumns": [
-                        {"bSearchable": false, "bSortable": false, "sWidth":"20px"},
-                        {"bSearchable": false, "bSortable": false},
-                        {"bSearchable": false, "bSortable": false, "sWidth":"150px"},
-                        {"bSearchable": false, "bSortable": false, "sWidth":"150px"},
-                        {"bSearchable": false, "bSortable": false, "sWidth":"100px"},
-                        {"bSearchable": false, "bSortable": false, "sWidth":"30px"}],
-                  "sAjaxSource": "<?php echo site_url().'/'.$url; ?>/get_datatable/",
-                  "autoWidth": false,
-                  "responsive": true
-         });   
+            "paging": true,
+            "iDisplayLength": 10,
+            "bProcessing": true,
+            "bServerSide": true,
+            'language': {
+                'processing': '<i class="fa fa-spinner fa-spin fa-1x fa-fw"></i><span> Processing...</span>'
+            },
+            "searching": false,
+            "aoColumns": [{
+                    "bSearchable": false,
+                    "bSortable": false,
+                    "sWidth": "20px"
+                },
+                {
+                    "bSearchable": false,
+                    "bSortable": false
+                },
+                {
+                    "bSearchable": false,
+                    "bSortable": false,
+                    "sWidth": "150px"
+                },
+                {
+                    "bSearchable": false,
+                    "bSortable": false,
+                    "sWidth": "150px"
+                },
+                {
+                    "bSearchable": false,
+                    "bSortable": false,
+                    "sWidth": "100px"
+                },
+                {
+                    "bSearchable": false,
+                    "bSortable": false,
+                    "sWidth": "30px"
+                }
+            ],
+            "sAjaxSource": "<?php echo site_url() . '/' . $url; ?>/get_datatable/",
+            "autoWidth": false,
+            "responsive": true
+        });
     });
+
+    function reloadTable() {
+        $('#table-tes').dataTable().fnReloadAjax();
+    }
 </script>
